820e72902d61c4e3811fac07ce505b02b83ad771
Dipakkamble <120355415+Dipakkamble@users.noreply.github.com>
2023-01-11 14:50:40 +0530

    Update Program.cs

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -12,11 +12,8 @@ namespace HelloWorldDemo
         {
             Console.WriteLine("Welcome and Hello Everyone");
             Console.WriteLine("Making changes in Remote Main");
-<<<<<<< HEAD
             Console.WriteLine("Creating conflict in local changes");
-=======
             Console.WriteLine("Creating conflict in remote changes");
->>>>>>> d0066a3aaac375383c0123bb6b3af90e00bf5a72
             Console.ReadLine();
         }
     }
